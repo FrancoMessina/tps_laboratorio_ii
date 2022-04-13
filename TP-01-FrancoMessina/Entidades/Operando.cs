3569ad6cd89c8aa7b57067b4369ff3f5e4368ef4
FrancoMessina <franco.m2017@outlook.com>
2022-04-13 19:26:05 -0300

    Borre carpeta para que quede más limpio

---
+++ TP-01-FrancoMessina/Entidades/Operando.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Text;
+namespace Entidades
+{
+    public class Operando
+    {
+        private double numero;
+
+        public Operando(double numero)
+        {
+            this.numero = numero;
+        }
+        public Operando() : this(0)
+        {
+
+        }
+        public Operando(string strNumero)
+        {
+            this.Numero = strNumero;
+        }
+
+        public string Numero
+        {
+            set { this.numero = ValidarOperando(value); }
+        }
+        private double ValidarOperando(string strNumero)
+        {
+            double numero;//Number
+            bool esNumero; //Is Number
+
+            esNumero = Double.TryParse(strNumero, out numero);
+            if (esNumero)
+                return numero;
+            return 0;
+        }
+
+        private bool EsBinario(string binario)
+        {
+            foreach (char valor in binario)
+            {
+                if (valor != '0' && valor != '1')
+                    return false;
+            }
+            return true;
+        }
+
+        public string BinarioDecimal(string binario)
+        {
+            double resultado = 0;
+            char[] binarioInvertido = binario.ToCharArray();
+            Array.Reverse(binarioInvertido);
+            int largoBinario = binario.Length;
+            int multiplicador = 1;
+            string errorMensaje = "Valor inválido";
+            if (EsBinario(binario))
+            {
+                for (int i = 0; i < largoBinario; i++)
+                {
+                    if (binarioInvertido[i] == '1')
+                    {
+                        int aux = (int.Parse(binarioInvertido[i].ToString())) * multiplicador;
+                        resultado += aux;
+                    }
+                    multiplicador *= 2;
+                }
+                return resultado.ToString();
+            }
+            return errorMensaje;
+        }
+        public string DecimalBinario(double numero)
+        {
+            StringBuilder sbBinario = new StringBuilder();
+            string binarioFinal;
+            int numeroEntero = (int)Math.Abs(numero);
+            char[] binarioInvertido;//En este char de array voy a invertir el binario
+            string errorMensaje = "Valor inválido";
+            if (numero == 0)
+                return "0";
+            else if (numero > 0)
+            {
+                do
+                {
+                    if (numeroEntero % 2 == 0)
+                        sbBinario.Append($"0");
+                    else
+                        sbBinario.Append("1");
+                    numeroEntero /= 2;
+
+                } while (numeroEntero > 0);
+                binarioInvertido = sbBinario.ToString().ToCharArray();//Paso el string builder a string y despues a  un array de char
+                Array.Reverse(binarioInvertido);//Aca lo invierto asi me queda el binario Final
+                binarioFinal = new string(binarioInvertido);//Por legibilidad creo una variable llamada binarioFinal
+                return binarioFinal;
+            }
+            return errorMensaje;
+        }
+        public string DecimalBinario(string numero)
+        {
+            string errorMensaje = "Valor inválido";
+            if (Double.TryParse(numero, out double numeroRetorno))
+                return DecimalBinario(numeroRetorno);
+
+            return errorMensaje;
+        }
+        public static double operator +(Operando n1, Operando n2)
+        {
+            return n1.numero + n2.numero;
+        }
+        public static double operator -(Operando n1, Operando n2)
+        {
+            return n1.numero - n2.numero;
+        }
+        public static double operator *(Operando n1, Operando n2)
+        {
+            return n1.numero * n2.numero;
+        }
+        public static double operator /(Operando n1, Operando n2)
+        {
+            if (n2.numero == 0)
+                return Double.MinValue;
+            return n1.numero / n2.numero;
+        }
+    }
+}
---
+++ TP-01-FrancoMessina/Vista/FormCalculadora.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Windows.Forms;
+using Entidades;
+using System.Drawing;
+namespace Vista
+{
+    public partial class FormCalculadora : Form
+    {
+        private const string mensajeError = "Error Dividir por 0";
+        public FormCalculadora()
+        {
+            InitializeComponent();
+        }
+        private void Limpiar()
+        {
+            this.txtNumero1.Text = string.Empty;
+            this.txtNumero2.Text = string.Empty;
+            this.cmbOperador.SelectedIndex = 0;
+            this.lblResultado.Text = "0";
+        }
+
+        private void btnLimpiar_Click(object sender, EventArgs e)
+        {
+            this.Limpiar();
+        }
+
+        private void FormCalculadora_Load(object sender, EventArgs e)
+        {
+            this.cmbOperador.Items.Add(" ");
+            this.cmbOperador.Items.Add("+");
+            this.cmbOperador.Items.Add("-");
+            this.cmbOperador.Items.Add("/");
+            this.cmbOperador.Items.Add("*");
+            this.lblResultado.Text = "0";
+            this.cmbOperador.SelectedIndex = 0;
+        }
+        private static double Operar(string numero1, string numero2, string operador)
+        {
+            Operando operadorUno = new Operando(numero1);
+            Operando operadorDos = new Operando(numero2);
+            Calculadora nuevaCalculadora = new Calculadora();
+            return nuevaCalculadora.Operar(operadorUno, operadorDos, Convert.ToChar(operador));
+        }
+
+        private void btnCerrar_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnOperar_Click(object sender, EventArgs e)
+        {
+            string numeroUno = this.txtNumero1.Text;
+            string numeroDos = this.txtNumero2.Text;
+            string operador = (string)cmbOperador.SelectedItem;
+            double resultadoCuenta;
+            bool errorNumeroUno = string.IsNullOrWhiteSpace(numeroUno);
+            bool errorNumeroDos = string.IsNullOrWhiteSpace(numeroDos);
+            string mensaje;
+
+            if (!errorNumeroUno && !errorNumeroDos && double.TryParse(numeroUno, out double numeroUnoErrorParse) && double.TryParse(numeroDos, out double numeroDosErrorParse) && !(numeroDos == "0" && operador == "/"))
+            {
+                resultadoCuenta = Operar(numeroUno, numeroDos, operador);
+                this.lblResultado.Text = resultadoCuenta.ToString();
+                //mensaje = operador == " " ? $"{numeroUno} + {numeroDos} = {resultadoCuenta}" : $"{numeroUno} {operador} {numeroDos} = {resultadoCuenta}";
+                if (operador == " ")
+                {
+                    mensaje = $"{numeroUno} + {numeroDos} = {resultadoCuenta}";
+                    cmbOperador.SelectedIndex = 1;
+                }
+                else
+                    mensaje = $"{numeroUno} {operador} {numeroDos} = {resultadoCuenta}";
+                this.lstOperaciones.Items.Add(mensaje);
+                this.lblResultado.ForeColor = Color.Black;
+                this.btnConvertirADecimal.Enabled = false;
+                this.btnConvertirABinario.Enabled = true;
+                
+            }
+            else if (errorNumeroUno && errorNumeroDos)
+                MessageBox.Show("Error. Necesitas cargar los dos Valores.","Error",MessageBoxButtons.OK,MessageBoxIcon.Warning);
+            else if (errorNumeroUno)
+                MessageBox.Show("Error. Necesitas cargar el primer Valor.","Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            else if (errorNumeroDos)
+                MessageBox.Show("Error. Necesitas cargar el segundo Valor.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            else if (numeroDos == "0" && operador == "/")
+            {
+                this.lblResultado.Text = mensajeError;
+                this.lblResultado.ForeColor = Color.Red;
+            }
+            else
+                MessageBox.Show("Ingresar un numero válido", "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+        }
+
+        private void btnConvertirABinario_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(lblResultado.Text) || lblResultado.Text == mensajeError)
+                MessageBox.Show("No hay resultado validado para convertir a Binario.");           
+            else
+            {
+                if (Double.TryParse(lblResultado.Text, out double resultado))
+                {
+                    Operando auxBinario = new Operando();
+                    this.lblResultado.Text = auxBinario.DecimalBinario(resultado); //Tambien podria pasar lblResultado.Text como param
+                    this.btnConvertirADecimal.Enabled = true;
+                    this.btnConvertirABinario.Enabled = false;
+                }
+                
+            }
+           
+        }
+
+        private void btnConvertirADecimal_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(lblResultado.Text) || lblResultado.Text == mensajeError)
+                MessageBox.Show("No hay resultado validado para convertir a Decimal.");
+            else
+            {
+                if (Double.TryParse(lblResultado.Text, out double resultado))
+                {
+                    Operando auxBinario = new Operando();
+                    this.lblResultado.Text = auxBinario.BinarioDecimal(lblResultado.Text); //Tambien podria pasar lblResultado.Text como param
+                }
+                this.btnConvertirADecimal.Enabled = false;
+                this.btnConvertirABinario.Enabled = true;
+            }
+            
+        }
+
+        private void FormCalculadora_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            DialogResult dialogRespuesta = MessageBox.Show("¿Seguro de queres salir?", "Salir", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (dialogRespuesta == DialogResult.Yes)
+                e.Cancel = false;
+            else
+                e.Cancel = true;
+        }
+    }
+}
